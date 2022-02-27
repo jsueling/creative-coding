89c69108194c65e4d9cd6e1210aff6d2cd09d088
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-02-27 15:04:15 +0000

    glyphs

--- unit6/sketch-06.js
+++ unit6/sketch-06.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random')
 
 const settings = {
   dimensions: [ 1080, 1080 ]
@@ -27,7 +28,7 @@ const sketch = ({ context, width, height }) => {
     typeContext.fillStyle = 'black';
     typeContext.fillRect(0, 0, width, height);
 
-    fontSize = cols
+    fontSize = cols * 1.2
 
     typeContext.fillStyle = 'white'
     typeContext.font = `${fontSize}px ${fontFamily}` // fontSize + 'px' + fontFamily
@@ -57,7 +58,14 @@ const sketch = ({ context, width, height }) => {
 
     const typeData = typeContext.getImageData(0, 0, cols, rows).data
 
-    // context.drawImage(typeCanvas, 0, 0)
+    context.drawImage(typeCanvas, 0, 0)
+
+    context.fillStyle = 'black'
+
+    context.textBaseline = 'middle'
+    context.textAlign = 'center'
+
+    context.fillRect(0,0,width,height)
 
     for (let i = 0; i < numCells; i++) {
       const col = i % cols
@@ -71,23 +79,45 @@ const sketch = ({ context, width, height }) => {
       const b = typeData[i * 4 + 2]
       // const a = typeData[i * 4 + 3]
 
-      context.fillStyle = `rgb(${r}, ${g}, ${b})`
+      const glyph = getGlyph(r) // only need to pass r for alternating between black and white
+
+      context.font = `${cell * 2}px ${fontFamily}`
+      if (Math.random() < 0.1) context.font = `${cell * 6}px ${fontFamily}`
+
+      context.fillStyle = 'white'
 
       context.save()
       context.translate(x, y)
       context.translate(cell*0.5, cell*0.5)
+
+      // squares
       // context.fillRect(0, 0, cell, cell)
 
-      context.beginPath()
-      context.arc(0,0,cell*0.5,0,Math.PI*2)
-      context.fill()
+      // circles
+      // context.beginPath()
+      // context.arc(0,0,cell*0.5,0,Math.PI*2)
+      // context.fill()
+
+      context.fillText(glyph, 0, 0)
+
       context.restore()
     }
   };
 };
 
+const getGlyph = (v) => { // v 0-255
+  if (v < 50) return ''
+  if (v < 100) return '.'
+  if (v < 150) return '-'
+  if (v < 200) return '+'
+
+  const glyphs = '_= /'.split('')
+
+  return random.pick(glyphs)
+}
+
 const onKeyUp = (e) => {
-  text = e.key//.toUpperCase()
+  text = e.key.toUpperCase()
   // if (e.key == 'Backspace') text.pop()
   // else if (e.key.length === 1) text.push(e.key)
   manager.render()
