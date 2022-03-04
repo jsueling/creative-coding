dd647fd8dfce512b35284d379b12c8874071baa8
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-03-04 13:15:07 +0000

    added tweakpane slider to filter agents by colour

--- unit6/fade-in-animation.js
+++ unit6/fade-in-animation.js
@@ -1,11 +1,16 @@
 const canvasSketch = require('canvas-sketch');
 const math = require('canvas-sketch-util/math')
 const random = require('canvas-sketch-util/random')
+const { Pane } = require('tweakpane')
 
 const settings = {
   dimensions: [ 1080, 1080 ],
 	animate: true
 };
+
+const params = {
+	redFilter: 0
+}
 
 let img
 
@@ -14,7 +19,7 @@ const typeContext = typeCanvas.getContext('2d');
 
 const sketch = ({ context, width, height }) => {
 
-  const cell = 5;
+  const cell = 7;
 	const cols = Math.floor(width  / cell);
 	const rows = Math.floor(height / cell);
 	const numCells = cols * rows;
@@ -76,7 +81,7 @@ const sketch = ({ context, width, height }) => {
 		const zoomEffect = math.mapRange(hyp, 0, width/2, 0.1, 30) // zoom effect, speed is mapped to distance from mid which is increasing with distance from mid
 		const mapRadius = math.mapRange(hyp, 200, width/2, 0.4, 0.9) // radius mapped to distance from mid, decreasing with distance from mid
 		const speedFactor = random.range(1, 3) // random speeds from mid  // zoomEffect
-		const radiusFactor = random.range(1, 1.3) // random radius of pixel // 1.5 - mapRadius 
+		const radiusFactor = 0.6 // random.range(1, 1.3) // random radius of pixel // 1.5 - mapRadius 
 
 		const velx =  speedFactor * Math.cos(angleFromMid) // x, y position from angle to get vel * radius (speed)
 		const vely =  -speedFactor * Math.sin(angleFromMid) // correct our inverted y axis *= -1
@@ -94,7 +99,7 @@ const sketch = ({ context, width, height }) => {
 
 		agents.forEach((a) => {
 			a.update(time)
-			a.draw(context, width, height)
+			a.draw(context, params.redFilter)
 		})
 
 		// context.fillStyle='red' // test reference point
@@ -104,7 +109,12 @@ const sketch = ({ context, width, height }) => {
   };
 };
 
-const url = 'https://i.picsum.photos/id/88/1080/1080.jpg?hmac=wXuxKrT3ddwEfKucdKkk9vuhT99Cc2bUSi5yqEk4wEY' // https://picsum.photos/1080
+const createPane = () => {
+	const pane = new Pane()
+	pane.addInput(params, 'redFilter', { min: 90, max: 255, step: 1 })
+}
+
+const url = 'https://i.picsum.photos/id/972/1080/1080.jpg?grayscale&hmac=gb0H_hVYBXNg9JXq5eC81eOe30suZZw0KVcyzh0lmgk' // https://picsum.photos/1080
 
 const loadImage = (url) => {
   return new Promise((res, rej) => {
@@ -118,6 +128,7 @@ const loadImage = (url) => {
 
 const start = async () => {
   img = await loadImage(url)
+	createPane()
 	canvasSketch(sketch, settings, img);
 }
 
@@ -140,38 +151,40 @@ class Agent {
 	}
 
 	update(time) {
-		const playhead = time % 11 // 0->11 loop
+		const playhead = time % 15 // 0->11 loop
 		// pause 1 seconds
-		if (playhead > 2 && playhead < 2.5) { // 0.5 growing at x4
+		if (playhead > 3 && playhead < 3.5) { // 0.5 growing at x4
 			this.pos.x += this.vel.x*4;
 			this.pos.y += this.vel.y*4;
 			// this.rad /= 0.99
-		}	else if (playhead > 9 && playhead < 10 ){ // 1 shrinking at x2
+		}	else if (playhead > 11 && playhead < 12 ){ // 1 shrinking at x2
 			this.pos.x -= this.vel.x*2;
 			this.pos.y -= this.vel.y*2;
 			// this.rad *= 0.99
-		} else if (playhead > 2.5 && playhead < 5.75) { // 3.25 growing at x1/4
+		} else if (playhead > 3.5 && playhead < 7.25) { // 3.25 growing at x1/4
 			this.pos.x += this.vel.x/4;
 			this.pos.y += this.vel.y/4;
-		} else if (playhead > 5.75 && playhead < 9) { // 3.25 shrinking at x1/4
+		} else if (playhead > 7.25 && playhead < 11) { // 3.25 shrinking at x1/4
 			this.pos.x -= this.vel.x/4;
 			this.pos.y -= this.vel.y/4;
-		} else if (playhead > 9) { // snaps back into start position and pause 1 seconds
+		} else if (playhead > 12) { // snaps back into start position and pause 1 seconds
 			this.pos.x = this.initPos.x
 			this.pos.y = this.initPos.y
 		}
 	}
 
-	draw(context) {
-		context.save();
-		context.translate(this.pos.x, this.pos.y) // translate to top left corner of cell
-		context.translate(this.rad, this.rad) // translate to mid
+	draw(context, redFilter) {
 		const { r, g, b } = this.rgb
-		context.fillStyle = `rgb(${r},${g},${b})` // colour
-		context.beginPath();
-		context.arc(0, 0, this.rad, 0, 2*Math.PI);
-		context.fill();
+		if (r > redFilter) {
+			context.save();
+			context.translate(this.pos.x, this.pos.y) // translate to top left corner of cell
+			context.translate(this.rad, this.rad) // translate to mid
+			context.fillStyle = `rgb(${r},${g},${b})` // colour
+			context.beginPath();
+			context.arc(0, 0, this.rad, 0, 2*Math.PI);
+			context.fill();
 
-		context.restore();
+			context.restore();
+		}
 	}
 }
